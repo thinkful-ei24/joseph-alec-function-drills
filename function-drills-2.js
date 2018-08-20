c20d68efbac2246a5378bd6ebf8e7b6be4c89e30
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-08-20 12:55:11 -0700

    to infinity

--- function-drills-2.js
+++ function-drills-2.js
@@ -6,3 +6,21 @@ const jediName = function(firstName, lastName){
 }
 
 console.log(jediName('john', 'smith'));
+
+const beyond = function(num){
+  if(num === Infinity || num === -Infinity){
+    return 'And beyond';
+  } else if(num > 0){
+    return 'to Infinity';
+  } else if(num < 0){
+    return 'to negative Infinity';
+  } else if(num === 0){
+    return 'Staying Home';
+  }
+}
+
+console.log('this should be And beyond: ' + beyond(-Infinity));
+console.log('this should be And beyond: ' + beyond(Infinity));
+console.log('this should be to Infinity: ' + beyond(5));
+console.log('this should be to negative Infinity: ' + beyond(-5));
+console.log('this should be to staying home : ' + beyond(0));
