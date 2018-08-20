b4314b23ee7110face5ce5ea7c838f6b04d281a6
josephhess <readysteadyco@gmail.com>
2018-08-20 11:57:34 -0700

    check for name and age

--- function-drills-1.js
+++ function-drills-1.js
@@ -7,13 +7,16 @@ const getYearOfBirth = function(age){
     return 2018 - age;
 }
 const createGreeting = function(name, age){
+    if (!name || !age){
+        throw new Error ('Arguments not valid');
+    }
     const yob = getYearOfBirth(age);
     return `Hi my name is ${name} and i am ${age} years old
      and I was born in ${yob}`;
 }
 
 try {
-    const greeting1 = createGreeting('joseph',44);
+    const greeting1 = createGreeting(joseph ,44);
     console.log(greeting1);
 } catch(e){
     console.log(e.message);
