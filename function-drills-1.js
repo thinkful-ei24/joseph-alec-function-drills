240246d281d42d21ddbf62986d6e480ef944d127
josephhess <readysteadyco@gmail.com>
2018-08-20 11:39:11 -0700

    takes variable for name and age

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,6 +1,6 @@
-const createGreeting = function(){
-    return 'Hi my name is Joseph';
+const createGreeting = function(name, age){
+    return `Hi my name is ${name} and i am ${age} years old`;
 }
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting('joseph',44);
 console.log(greeting1);
