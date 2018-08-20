071df9a10ec4b10d2ce8e01356db9c5069722574
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-08-20 13:18:58 -0700

    code cracker

--- function-drills-2.js
+++ function-drills-2.js
@@ -19,8 +19,48 @@ const beyond = function(num){
   }
 }
 
-console.log('this should be And beyond: ' + beyond(-Infinity));
-console.log('this should be And beyond: ' + beyond(Infinity));
-console.log('this should be to Infinity: ' + beyond(5));
-console.log('this should be to negative Infinity: ' + beyond(-5));
-console.log('this should be to staying home : ' + beyond(0));
+// console.log('this should be And beyond: ' + beyond(-Infinity));
+// console.log('this should be And beyond: ' + beyond(Infinity));
+// console.log('this should be to Infinity: ' + beyond(5));
+// console.log('this should be to negative Infinity: ' + beyond(-5));
+// console.log('this should be to staying home : ' + beyond(0));
+
+const decodeSentence = function(str){
+  const words = str.split(" ");
+  console.log(words);
+  let newStr = '';
+  for(let word of words){
+    newStr+=decode(word);
+  }
+  return newStr;
+}
+
+const decode = function(word){
+  let countTo = letToNum(word[0]);
+  if(countTo === 0){
+    return ' ';
+  }
+  return word[countTo-1];
+}
+
+
+const letToNum = function(letter){
+  switch(letter){
+    case 'a':
+      return 2;
+      break;
+    case 'b':
+      return 3;
+      break;
+    case 'c':
+      return 4;
+      break;
+    case 'd':
+      return 5;
+      break;
+    default: return 0;
+  }
+}
+
+console.log('this should be an l: ' + decode('cycle'));
+console.log(decodeSentence('craft block argon meter bells brown croon droop'));
