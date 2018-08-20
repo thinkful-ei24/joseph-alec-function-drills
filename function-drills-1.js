ba97eba541c0b87792f471aaab7dba2a35e4b869
josephhess <readysteadyco@gmail.com>
2018-08-20 11:45:14 -0700

    calculates year of birth

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,5 +1,9 @@
+'use strict';
+
 const createGreeting = function(name, age){
-    return `Hi my name is ${name} and i am ${age} years old`;
+    const yearOfBirth = 2018 - age;
+    return `Hi my name is ${name} and i am ${age} years old
+     and I was born in ${yearOfBirth}`;
 }
 
 const greeting1 = createGreeting('joseph',44);
