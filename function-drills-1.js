d74c4efa7ff244b3d5b1131012e21a9746fde10d
josephhess <readysteadyco@gmail.com>
2018-08-20 11:47:56 -0700

    getYearOfBirth function

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const getYearOfBirth = function(age){
+    return 2018 - age;
+}
 const createGreeting = function(name, age){
-    const yearOfBirth = 2018 - age;
+    const yob = getYearOfBirth(age);
     return `Hi my name is ${name} and i am ${age} years old
-     and I was born in ${yearOfBirth}`;
+     and I was born in ${yob}`;
 }
 
 const greeting1 = createGreeting('joseph',44);
